3804772be685d9fa217cf3eb97818d5f14d0dad9
Tavva Prudhvith <38600870+ChillBoss@users.noreply.github.com>
2019-09-28 12:57:15 +0530

    commited

--- addi.py
+++ addi.py
@@ -1,4 +1,4 @@
 def add(a,b):
         res = int(a) + int(b)
         print("Addition of the numbers is :"+str(res) )
-        
+        print(a+b)
--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import sys
 
 print("Enter the operation to perform")
 print("1: Addition\n 2: Subtraction ")
-inp = input()
+#inp = input()
 
-print("Enter the values")
-i1 = input("A:")
-i2 = input("B:")
-
-if (inp==1):
-        addi.add(i1,i2)
-if (inp==2):
-        sub.subtract(i1,i2)
+#print("Enter the values")
+i1 = 2
+i2 = 3
+#if (inp==1):
+#import addi
+addi.add(i1,i2)
+#if (inp==2):
+sub.subtract(i1,i2)
 #else:
  #       print("Please select between 1 to 2 only")
   #      sys.exit(0)      
